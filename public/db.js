027201b82eb3e819f7f74c120fa68f4710dd7f4c
aydanegash <73369360+aydanegash@users.noreply.github.com>
2021-08-27 08:28:02 -0500

    db.js

--- public/db.js
+++ public/db.js
@@ -1,26 +1,54 @@
-// if('servicewoker' in navigator){
-//     navigator.serviceWorker.register('/service-worker.js')
-//     .then((reg) => console.log('service worker registered', reg))
-//     .catch((err) => console.log('service worker NOT registered', err))
-// }
+let db;
+let budget;
+  
+    request.onupgradeneeded = function(e) {
+        const db = request.result;
+        db.createObjectStore(pending, { autoIncrement: true });
+    };
+  
+    request.onerror = function(e) {
+        console.log(`${target.errorCode}`);
+        function saveRecord(record) {
+            const transaction = db.transaction(['budget'], 'readwrite');
+            const store = transaction.objectStore('budget');
+            store.add(record);
+          }
+    };
+  
+    request.onsuccess = function(event) {
+        db = db = event.target.result;
+  
+    db.onerror = function(e) {
+        console.log(`error ${event.target.errorCode}`);
+    };
 
-document.addEventListener('DOMContentLoaded', init, false);
-function init() {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then((reg) => {
-        console.log('Service worker registered -->', reg);
-      }, (err) => {
-        console.error('Service worker not registered -->', err);
-      });
-  }
-}
 
-document.addEventListener('DOMContentLoaded', init, false);
 
-function init() {
-  if (!navigator.onLine) {
-    const statusElem = document.querySelector('.page-status')
-    statusElem.innerHTML = 'offline'
-  }
-}
+  function checkDatabase() {
+        const transaction = db.transaction(["pending"], "readwrite");
+        const store = transaction.objectStore("pending");
+        const getAll = store.getAll();
+
+
+        getAll.onsuccess = function () {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                Accept: 'application/json, text/plain, */*',
+                'Content-Type': 'application/json',
+                },
+            })
+                .then((response) => response.json())
+                .then((res) => {
+                    transaction = db.transaction(['budget'], 'readwrite');
+                    const newStore = transaction.objectStore('budget');
+                    currentStore.clear();
+                    console.log("store is clear");    
+
+            });
+        }
+      };
+    }
+
+    window.addEventListener('online', checkDatabase);
